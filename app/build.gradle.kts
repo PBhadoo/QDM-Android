effa0201d06cd747a712558d2b5bbeb5fd6855e9
Hash Hackers <87309724+PBhadoo@users.noreply.github.com>
2026-03-28 20:08:19 +0530

    chore: set version to 1.0.0 (versionCode 1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.parveenbhadoo.qdm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
